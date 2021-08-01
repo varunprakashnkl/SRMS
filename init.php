36fdc913bf97aab250b1fe87eac99fc439eae980
Varunprakash S <48172544+varunprakashnkl@users.noreply.github.com>
2021-08-01 18:26:40 +0530

    Add files via upload

---
+++ init.php
@@ -0,0 +1,9 @@
+<?php
+	$servername = "localhost";
+	$username = "unistamv";
+	$password = "Varunnkl01@";
+	$conn = mysqli_connect($servername, $username, $password);
+	$db = mysqli_select_db($conn,'unistam');
+
+
+?>
